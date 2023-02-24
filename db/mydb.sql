909c8c20a22c99a74b3fe258fb26cbfafea7c2ad
Conner Christ <cchrist@sermonaudio.com>
2023-02-23 22:01:48 -0500

    progress

--- db/mydb.sql
+++ db/mydb.sql
@@ -1,7 +1,10 @@
-create table Person (
-  person_id integer primary key,
-  person_name varchar(30)
+create table Registrations (
+  username varchar(30) primary key,
+  first_name varchar(30),
+  last_name varchar(30),
+  grade integer,
+  email varchar(50),
+  size varchar(1),
 );
 
-insert into Person values (1, 'Fred Jones');
-insert into Person values (2, 'Alf Wilson');
+insert into Registrations values (1, 'Fred', 'Jones', 4, 'test', 'M', 'testtest');
--- webapp/app.js
+++ webapp/app.js
@@ -1,4 +1,5 @@
 let express = require("express");
+let bodyParser = require("body-parser");
 
 let mysql = require("mysql");
 
@@ -16,8 +17,10 @@ function getConnection() {
 }
 
 let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/hello', function (req, res) {
+app.get('/registrations', function (req, res) {
     let connection = getConnection();
     connection.connect(function (err) {
         if (err) {
@@ -26,8 +29,50 @@ app.get('/hello', function (req, res) {
             return;
         }
         console.log("Successful connection!");
-        connection.query("SELECT * FROM Person", function (err, results) {
+        connection.query("SELECT * FROM Registrations", function (err, results) {
             res.send(results);
+            connection.destroy();
+        });
+    });    
+});
+
+app.post('/registrations', function (req, res) {
+    let connection = getConnection();
+    connection.connect(function (err) {
+        if (err) {
+            console.log("Problem connecting to database", err);
+            res.status(500).send("Unable to connect to database! " + err);
+            return;
+        }
+        console.log("Successful connection!");
+        
+        let reg = req.body;
+        if (!(reg.firstName && reg.lastName && reg.grade && reg.email && reg.shirtSize && reg.hrUsername)) {
+            res.status(400).send("Validation error, missing required field(s).");
+            connection.destroy();
+            return;
+        }
+        if (['S', 'M', 'L'].indexOf(reg.shirtSize) < 0) {
+            res.status(400).send("Validation error, bad shirt size (S, M, or L).");
+            connection.destroy();
+            return;
+        } 
+        if (reg.grade < 9 || reg.grade > 12) {
+            res.status(400).send("Validation error, bad grade (9, 10, 11, or 12).");
+            connection.destroy();
+            return;
+        }
+
+        let insert = `INSERT INTO Registrations VALUES ('${reg.hrUsername}','${reg.firstName}','${reg.lastName}','${reg.grade}','${reg.email}','${reg.shirtSize}')`
+
+        connection.query(insert, function (err, results) {
+            if (err) {
+                res.status(500).send("Unable to insert record! " + err);
+                connection.destroy();
+                return;
+            }
+
+            res.status(200).send(results);
             connection.destroy();
         });
     });    
